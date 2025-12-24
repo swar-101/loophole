5a5b84e215a530630647f84776993951ac8c3a6a
swar-101 <kunwarswarprakash@gmail.com>
2025-12-24 09:51:01 +0530

    Add component: ProblemViewer

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,10 @@
 import ProblemWorkspace from "./components/workspace/ProblemWorkspace.jsx";
+import ProblemViewer from "./components/ProblemViewer.jsx";
+import problemData from "./../docs/sample-problem.json"
 
 function App() {
-   return <ProblemWorkspace />;
+    // return <ProblemWorkspace />;
+    return <ProblemViewer problem={problemData}/>
 }
 
 export default App;
